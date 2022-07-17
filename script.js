6bfb6534a0bcd5d9c8aa1ef328de864de4315f25
palmer2539 <alexandr.palmer.dev@gmail.com>
2022-07-17 14:30:58 -0400

    changed some organization and worked a little on JS, but having some trouble

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-console.log('test1')
+console.log('powered on')
 
-function buttonClick(value) {
-    console.log(value)
-}
+// var OTLandingPageInstance = document.querySelector('landingPageOT')
+// var OTLandingPageStyle = getComputedStyle(OTLandingPageInstance)
+// console.log(OTLandingPageStyle)
 
-function(){
-    
-}
+// function startButtonToggleHS() {
+//     var OTLandingPageInstance = document.querySelector('.landingPageOT')
+
+//     var OTLandingPageStyle = getComputedStyle(OTLandingPageInstance)
+
+//     console.log(OTLandingPageStyle)
+
+//     var x = OTLandingPageStyle;
+//      if (x.style.display === "block") {
+//         x.style.display = "none";
+//     } else {
+//         x.style.display = "block";
+//     }
+//     console.log('worked')
+// }
+
+
+
+// function startButtonToggleHS() {
+//     var x = document.getElementsByClassName("landingPageOT");
+//     var y = 'block'
+//      if (y === "block") {
+//         x.style.display = "none";
+//     } else {
+//         x.style.display = "block";
+//     }
+//     console.log('worked')
+// }
