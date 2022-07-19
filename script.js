1d864063417d48c27ebf361cc84d19f79efa5994
palmer2539 <alexandr.palmer.dev@gmail.com>
2022-07-18 22:22:39 -0400

    worked on design and fixed js

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 console.log('powered on')
 
-// var OTLandingPageInstance = document.querySelector('landingPageOT')
-// var OTLandingPageStyle = getComputedStyle(OTLandingPageInstance)
-// console.log(OTLandingPageStyle)
 
-// function startButtonToggleHS() {
-//     var OTLandingPageInstance = document.querySelector('.landingPageOT')
-
-//     var OTLandingPageStyle = getComputedStyle(OTLandingPageInstance)
-
-//     console.log(OTLandingPageStyle)
-
-//     var x = OTLandingPageStyle;
-//      if (x.style.display === "block") {
-//         x.style.display = "none";
-//     } else {
-//         x.style.display = "block";
-//     }
-//     console.log('worked')
-// }
-
-
-
-function startButtonToggleHS() {
-    var x = document.getElementsByClassName("landingPageOT");
-    if (x === "block") {
-        x.style.display = "none";
-    } else {
+function OTstartButtonToggleHS() {
+    var x = document.getElementById("landingPageOT");
+    if (x === "none") {
         x.style.display = "block";
+    } else {
+        x.style.display = "none";
     }
     console.log('worked')
+}
+
+function NTstartButtonToggleHS() {
+  var x = document.getElementById("landingPageNT");
+  if (x === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+  console.log('worked')
+}
+
+function HBstartButtonToggleHS() {
+  var x = document.getElementById("landingPageNT");
+  if (x === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+  console.log('worked')
+}
+
+function OTgo() {
+  window.location.href = "/triviaPages/old_testament.html";
+  console.log('workedd')
 }
