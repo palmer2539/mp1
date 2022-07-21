13b45044cb6fb8c07aa383b575ab291e3466c97b
palmer2539 <alexandr.palmer.dev@gmail.com>
2022-07-21 00:14:23 -0400

    css for questions

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 console.log('powered on')
-
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Hides Old Testament landing page
@@ -16,9 +14,8 @@ function OTstartButtonHideHS() {
     }
     console.log('hiding landing page worked');
 }
-
 ////////////////////////////////////
-
+////////////////////////////////////
 //On button click, shows 'next button 1'
 function showNextButton1OT() {
     var x = document.getElementById("nextButton1OT");
@@ -44,9 +41,8 @@ function showNextButton5OT() {
     var x = document.getElementById("nextButton5OT");
     x.style.display = "block";
 }
-
 ////////////////////////////////////
-
+////////////////////////////////////
 //hides next button, hides question 1, shows question 2
 function hideNextButton1OT() {
     var x = document.getElementById("nextButton1OT");
@@ -140,13 +136,9 @@ function hideFinalScreenOT() {
     }
     console.log('hiding landing page worked');
 }
-
-
-
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//New Testament start button hide
+//Hides New Testament landing page
 function NTstartButtonHideHS() {
     var x = document.getElementById("landingPageNT");
     var y = document.getElementById("question1NT");
@@ -159,9 +151,8 @@ function NTstartButtonHideHS() {
     }
     console.log('hiding landing page worked');
 }
-
 ////////////////////////////////////
-
+////////////////////////////////////
 //On button click, shows 'next button 1'
 function showNextButton1NT() {
     var x = document.getElementById("nextButton1NT");
@@ -187,9 +178,8 @@ function showNextButton5NT() {
     var x = document.getElementById("nextButton5NT");
     x.style.display = "block";
 }
-
 ////////////////////////////////////
-
+////////////////////////////////////
 //hides next button, hides question 1, shows question 2
 function hideNextButton1NT() {
     var x = document.getElementById("nextButton1NT");
@@ -238,7 +228,6 @@ function hideNextButton3NT() {
     console.log('hid next button, hid question 3, showed question 4');
 }
 
-
 //hides next button, hides question 4, shows question 5
 function hideNextButton4NT() {
     var x = document.getElementById("nextButton4NT");
@@ -284,21 +273,142 @@ function hideFinalScreenNT() {
     }
     console.log('hiding landing page worked');
 }
-
-
-
-
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Bible History start button hide
+//Hides Bible History landing page
 function HBstartButtonHideHS() {
-  var x = document.getElementById("landingPageHB");
-  if (x === "none") {
-      x.style.display = "block";
-  } else {
-      x.style.display = "none";
-  }
-  console.log('worked')
+    var x = document.getElementById("landingPageHB");
+    var y = document.getElementById("question1HB");
+    if (x === "none", y ==="block") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+    console.log('hiding landing page worked');
+}
+////////////////////////////////////
+////////////////////////////////////
+//On button click, shows 'next button 1'
+function showNextButton1HB() {
+    var x = document.getElementById("nextButton1HB");
+    x.style.display = "block";
+}
+//On button click, shows 'next button 2'
+function showNextButton2HB() {
+    var x = document.getElementById("nextButton2HB");
+    x.style.display = "block";
+}
+//On button click, shows 'next button 3'
+function showNextButton3HB() {
+    var x = document.getElementById("nextButton3HB");
+    x.style.display = "block";
+}
+//On button click, shows 'next button 4'
+function showNextButton4HB() {
+    var x = document.getElementById("nextButton4HB");
+    x.style.display = "block";
+}
+//On button click, shows 'next button 5'
+function showNextButton5HB() {
+    var x = document.getElementById("nextButton5HB");
+    x.style.display = "block";
+}
+////////////////////////////////////
+////////////////////////////////////
+//hides next button, hides question 1, shows question 2
+function hideNextButton1HB() {
+    var x = document.getElementById("nextButton1HB");
+    x.style.display = "none";
+    var y = document.getElementById("question1HB");
+    var z = document.getElementById("question2HB");
+    if (y === "none", z ==="block") {
+        y.style.display = "block";
+        z.style.display = "none";
+    } else {
+        y.style.display = "none";
+        z.style.display = "block";
+    }
+    console.log('hid next button, hid question 1, showed question 2');
 }
 
+//hides next button, hides question 2, shows question 3
+function hideNextButton2HB() {
+    var x = document.getElementById("nextButton2HB");
+    x.style.display = "none";
+    var y = document.getElementById("question2HB");
+    var z = document.getElementById("question3HB");
+    if (y === "none", z ==="block") {
+        y.style.display = "block";
+        z.style.display = "none";
+    } else {
+        y.style.display = "none";
+        z.style.display = "block";
+    }
+    console.log('hid next button, hid question 2, showed question 3');
+}
+
+//hides next button, hides question 3, shows question 4
+function hideNextButton3HB() {
+    var x = document.getElementById("nextButton3HB");
+    x.style.display = "none";
+    var y = document.getElementById("question3HB");
+    var z = document.getElementById("question4HB");
+    if (y === "none", z ==="block") {
+        y.style.display = "block";
+        z.style.display = "none";
+    } else {
+        y.style.display = "none";
+        z.style.display = "block";
+    }
+    console.log('hid next button, hid question 3, showed question 4');
+}
+
+//hides next button, hides question 4, shows question 5
+function hideNextButton4HB() {
+    var x = document.getElementById("nextButton4HB");
+    x.style.display = "none";
+    var y = document.getElementById("question4HB");
+    var z = document.getElementById("question5HB");
+    if (y === "none", z ==="block") {
+        y.style.display = "block";
+        z.style.display = "none";
+    } else {
+        y.style.display = "none";
+        z.style.display = "block";
+    }
+    console.log('hid next button, hid question 4, showed question 5');
+}
+
+//hides next button, hides question 5, shows final screen
+function hideNextButton5HB() {
+    var x = document.getElementById("nextButton5HB");
+    x.style.display = "none";
+    var y = document.getElementById("question5HB");
+    var z = document.getElementById("finalScreenHB");
+    if (y === "none", z ==="block") {
+        y.style.display = "block";
+        z.style.display = "none";
+    } else {
+        y.style.display = "none";
+        z.style.display = "block";
+    }
+    console.log('hid next button, hid question 4, showed question 5');
+}
+
+//Hides final screen and shows landing page
+function hideFinalScreenHB() {
+    var x = document.getElementById("finalScreenHB");
+    var y = document.getElementById("landingPageHB");
+    if (x === "none", y ==="block") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+    console.log('hiding landing page worked');
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
